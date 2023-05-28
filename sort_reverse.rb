08d9765cd01f210a1ec162cec509de7d37c07ed3
Md. Ismail <mdismailcse26@gmail.com>
2023-05-28 17:14:55 +0600

    add

---
+++ sort_reverse.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+
+def alphabetize(arr, rev=false)
+  if rev
+    arr.sort { |item1, item2| item2 <=> item1 }
+	#puts 1
+	#puts arr
+  else
+    arr.sort { |item1, item2| item1 <=> item2 }
+  end
+end
+
+books = ["Heart of Darkness", "Code Complete", "The Lorax", "The Prophet", "Absalom, Absalom!"]
+
+puts "A-Z: #{alphabetize(books)}"
+puts "Z-A: #{alphabetize(books, true)}"
+
+
+#alphabetize(books)
+
+#puts 
